537df3224f6f5e42fa7e493747d4c2af5aa1d262
Yan PIERRU <yan.pierru@laposte.net>
2014-03-05 15:43:08 +0100

    Test swipe sur map

--- ProjetEclipse/DrawMyWayBeta4/src/com/ironrabbit/drawmywaybeta4/activity/CreateModifyRoute.java
+++ ProjetEclipse/DrawMyWayBeta4/src/com/ironrabbit/drawmywaybeta4/activity/CreateModifyRoute.java
@@ -7,7 +7,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
@@ -18,7 +17,6 @@ import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuItem.OnMenuItemClickListener;
-import com.actionbarsherlock.view.SubMenu;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -114,7 +112,7 @@ public class CreateModifyRoute extends SherlockActivity {
 			}
 		});
 		
-		getSupportActionBar().setTitle(mRoute.getName());
+		getActionBar().setTitle(mRoute.getName());
 
 	}
 	
